4b04f50f644891e44a42970d5730c12f576c16aa
bargest <DIZz92@mail.ru>
2023-11-27 00:04:36 +0300

    add all

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees(
+	employee_id SERIAL PRIMARY KEY,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	title VARCHAR(100),
+	birth_date DATE,
+	bio TEXT
+);
+SELECT * FROM employees;
+
+CREATE TABLE customers(
+	customer_id VARCHAR(10) PRIMARY KEY,
+    customer_name VARCHAR(100),
+    contact_name VARCHAR(100)
+);
+SELECT * FROM customers;
+
+CREATE TABLE orders(
+	order_id INTEGER,
+    customer_id VARCHAR(255) REFERENCES customers(customer_id),
+    employee_id INTEGER REFERENCES employees(employee_id),
+    order_date DATE,
+    ship_city VARCHAR(255)
+);
+SELECT * FROM orders;
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,44 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+import os
+
+password_sql = os.getenv('PASSWORD_SQL')
+
+with psycopg2.connect(host='localhost', database='north', user='postgres', password=password_sql) as conn:
+    with open('north_data/customers_data.csv', encoding='utf-8') as file:
+        csv.field_size_limit(10 ** 8)
+        reader = csv.reader(file)
+        next(reader)
+        with conn.cursor() as cur:
+            for i in reader:
+                try:
+                    cur.execute('INSERT INTO customers VALUES (%s, %s, %s)',
+                                (i[0], i[1], i[2]))
+                finally:
+                    conn.commit()
+
+    with open('north_data/employees_data.csv', encoding='utf-8') as file:
+        csv.field_size_limit(10 ** 8)
+        reader = csv.reader(file)
+        next(reader)
+        with conn.cursor() as cur:
+            for i in reader:
+                try:
+                    cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)',
+                                (i[0], i[1], i[2], i[3], i[4], i[5]))
+                finally:
+                    conn.commit()
+
+    with open('north_data/orders_data.csv', encoding='utf-8') as file:
+        csv.field_size_limit(10 ** 8)
+        reader = csv.reader(file)
+        next(reader)
+        with conn.cursor() as cur:
+            for i in reader:
+                try:
+                    cur.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)',
+                                (i[0], i[1], i[2], i[3], i[4]))
+                finally:
+                    conn.commit()
+conn.close()
